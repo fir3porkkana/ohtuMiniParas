c760a773546a492fe6bac5afc8398282a5314f7a
Kevhann <kevin.hann@helsinki.fi>
2019-11-22 11:53:32 +0200

    added book and bookmarks objects

---
+++ src/main/java/ohtu/Book.java
@@ -0,0 +1,31 @@
+package ohtu;
+
+public class Book {
+  private String author;
+  private String title;
+
+  public Book(String title, String author) {
+    this.title = title;
+    this.author = author;
+
+  }
+
+  /**
+   * @return the author
+   */
+  public String getAuthor() {
+    return author;
+  }
+
+  /**
+   * @return the title
+   */
+  public String getTitle() {
+    return title;
+  }
+
+  @Override
+  public String toString() {
+    return title + " by: " + author;
+  }
+}
---
+++ src/main/java/ohtu/Bookmarks.java
@@ -0,0 +1,26 @@
+package ohtu;
+
+import java.util.HashSet;;
+
+public class Bookmarks {
+  private HashSet<Book> bookmarks;
+
+  public Bookmarks() {
+    this.bookmarks = new HashSet<>();
+  }
+
+  public void addBookmark(Book book) {
+    bookmarks.add(book);
+  }
+
+  public void removeBookmark(Book book) {
+    bookmarks.remove(book);
+  }
+
+  /**
+   * @return the bookmarks
+   */
+  public HashSet<Book> getBookmarks() {
+    return bookmarks;
+  }
+}
