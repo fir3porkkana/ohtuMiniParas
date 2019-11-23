4793c7bfc3096974e77d1acb83e45ef7e6bf4443
Kevhann <kevin.hann@helsinki.fi>
2019-11-23 17:15:12 +0200

    made basic ui for book adding and viewing

--- src/main/java/ohtu/Main.java
+++ src/main/java/ohtu/Main.java
@@ -5,44 +5,85 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.GridPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage ikkuna) {
-        TextField vasenTeksti = new TextField();
-        vasenTeksti.setId("vasen_teksti");
-        TextField oikeaTeksti = new TextField();
-        oikeaTeksti.setId("oikea_teksti");
-        Button nappi = new Button("Kopioi");
-        nappi.setId("nappi");
+    public void start(Stage stage) {
 
-        nappi.setOnAction((event) -> {
-            oikeaTeksti.setText(vasenTeksti.getText());
+        Bookmarks bookmarks = new Bookmarks();
+        bookmarks.addBookmark(new Book("1984", "Georrge Orwell"));
+        bookmarks.addBookmark(new Book("Of mice and men", "John Steinbeck"));
+
+        Label authorLabel = new Label("Author");
+        Label titleLabel = new Label("Title");
+
+        TextField authorInput = new TextField();
+        authorInput.setId("author_input");
+        authorInput.setPromptText("Author");
+
+        TextField titleInput = new TextField();
+        titleInput.setId("title_input");
+        titleInput.setPromptText("Title");
+
+        Button submitButton = new Button("Add book");
+        submitButton.setId("submit_button");
+
+        GridPane gridPane = new GridPane();
+
+        // Setting size for the pane
+        gridPane.setMinSize(400, 200);
+
+        // Setting the padding
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+
+        // Setting the vertical and horizontal gaps between the columns
+        gridPane.setVgap(5);
+        gridPane.setHgap(5);
+
+        VBox bookList = new VBox();
+        bookList.setSpacing(10);
+
+        bookmarks.getBookmarks().forEach(book -> {
+            Label bookLabel = new Label(book.toString());
+            bookList.getChildren().add(bookLabel);
         });
 
-        HBox komponenttiryhma = new HBox();
-        komponenttiryhma.setSpacing(20);
-        komponenttiryhma.getChildren().addAll(vasenTeksti, nappi, oikeaTeksti);
+        // Arranging all the nodes in the grid
+        gridPane.add(titleLabel, 0, 0);
+        gridPane.add(titleInput, 1, 0);
+        gridPane.add(authorLabel, 0, 1);
+        gridPane.add(authorInput, 1, 1);
+        gridPane.add(submitButton, 0, 2);
+        gridPane.add(bookList, 0, 3);
 
-        Scene nakyma = new Scene(komponenttiryhma);
+        submitButton.setOnAction((event) -> {
+            Book book = new Book(titleInput.getText(), authorInput.getText());
+            bookmarks.addBookmark(book);
 
-        ikkuna.setScene(nakyma);
-        ikkuna.show();
+            bookList.getChildren().add(new Label(book.toString()));
+            authorInput.setText("");
+            titleInput.setText("");
+        });
+
+        Scene scene = new Scene(gridPane);
+
+        stage.setScene(scene);
+        stage.setHeight(500);
+        stage.setWidth(700);
+        stage.show();
     }
 
     public static void main(String[] args) {
         launch(Main.class);
 
-        Bookmarks bookmarks = new Bookmarks();
-        Book book = new Book("1984", "Orwell");
-
-        bookmarks.addBookmark(book);
-
-        bookmarks.getBookmarks().forEach(b -> System.out.println(b.toString()));
+        // bookmarks.getBookmarks().forEach(b -> System.out.println(b.toString()));
     }
 
 }
